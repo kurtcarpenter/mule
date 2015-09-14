c49565bf07804c746ea72b9b77271019b81b50f1
sgundlapalli11 <sai.g1996@gmail.com>
2015-09-14 16:14:36 -0400

    Update PlayerConfigController.java

--- src/main/java/team/screens/instances/PlayerConfigController.java
+++ src/main/java/team/screens/instances/PlayerConfigController.java
@@ -7,10 +7,20 @@ import team.screens.AScreen;
 
 public class PlayerConfigController extends AScreen {
 
+    @FXML
+	// fx:id="configPlayersButton"
+	private Button next;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        //who knows?
+        configPlayersButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                parent.displayScreen(MainApp.BLANK_SCREEN);
+            }
+        };
     }
+
 
     /* Example method to navigate to a different screen
     @FXML
