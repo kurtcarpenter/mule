96c80874c23b75d79593b5b06f2118faf17e50a9
sgundlapalli11 <sai.g1996@gmail.com>
2015-09-15 15:47:30 -0400

    Update PlayerConfigController.java

--- src/main/java/team/screens/instances/PlayerConfigController.java
+++ src/main/java/team/screens/instances/PlayerConfigController.java
@@ -2,8 +2,15 @@ package team.screens.instances;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 
 import team.screens.AScreen;
+import team.MainApp;
 
 public class PlayerConfigController extends AScreen {
 
