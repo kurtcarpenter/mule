30d89d22cecc8d97053c3959eb700350eddd144d
Kunaal Naik <laanuk13@gmail.com>
2015-09-21 18:52:52 -0400

    landSelect

--- src/main/java/team/Game.java
+++ src/main/java/team/Game.java
@@ -1,15 +1,18 @@
 package team;
 
 import team.config.Configuration;
+import team.game.LandSelectManager;
 import team.game.TurnManager;
 
 public class Game {
   private final Configuration configuration;
   private final TurnManager turnManager;
+  private final LandSelectManager landSelectManager;
 
   public Game(Configuration config) {
     configuration = config;
     turnManager = new TurnManager(configuration.getPlayers());
+    landSelectManager = new LandSelectManager(turnManager);
   }
 
   public Configuration getConfiguration() {
@@ -18,5 +21,9 @@ public class Game {
 
   public TurnManager getTurnManager() {
     return turnManager;
+  }
+
+  public LandSelectManager getLandSelectManager() {
+    return landSelectManager;
   }
 }
---
+++ src/main/java/team/game/LandSelectManager.java
@@ -0,0 +1,10 @@
+import team.game;
+
+public class LandSelectManager {
+
+  private TurnManager turnManager;
+
+  public LandSelectManager(TurnManager turnManager) {
+    this.turnManager = turnManager;
+  }
+}
