077525d3e840db9d03ab7774de835bade827b043
kurtcarpenter <kurtcarpenter@hotmail.com>
2015-09-21 21:00:59 -0400

    Player persist
    fix initial display

--- src/main/java/team/game/TurnManager.java
+++ src/main/java/team/game/TurnManager.java
@@ -12,7 +12,6 @@ public class TurnManager {
 
   public TurnManager(List<Player> players, GameState currentState) {
     this.players = players;
-    this.playerCount = players.size();
     this.turn = 1;
     this.step = 0;
 
@@ -33,8 +32,9 @@ public class TurnManager {
    * @param steps Number of steps to advance
    */
   public void advanceStep(int steps) {
+    System.out.println(step);
     step += steps;
-    turn = step / playerCount + 1;
+    turn = step / players.size() + 1;
   }
 
   /**
@@ -50,12 +50,12 @@ public class TurnManager {
    * Used for skipping a bunch of time into the future.
    */
   public void advanceTurn(int turns) {
-    step += playerCount * turns;
+    step += players.size() * turns;
     turn += turns;
   }
 
   public Player getCurrentPlayer() {
-    return players.get(step % playerCount);
+    return players.get(step % players.size());
   }
 
   public int getCurrentStep() {
