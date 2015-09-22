617e3c719cfe1f38e997593bae6a1f109f21a1ee
Srivas Sundar <srivas.srihari@gmail.com>
2015-09-21 20:11:50 -0400

    Created MainMap Base

--- src/main/java/team/screens/instances/MainMapController.java
+++ src/main/java/team/screens/instances/MainMapController.java
@@ -10,6 +10,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.scene.layout.GridPane;
 
 import team.screens.AScreen;
 import team.MainApp;
@@ -20,17 +21,66 @@ import team.screens.AScreen;
 public class MainMapController extends AScreen {
 
     @FXML
-    private Button townButton;
+    private GridPane mapGrid;
+
+    private String mapLayout[][] = { {"P","P","M1","P","R","P","M3","P","P"},
+        {"P","M1","P","P","R","P","P","P","M3"}, {"M3","P","P","P","Town","P","P","P","M1"},
+        {"P","M2","P","P","R","P","M2","P","P"}, {"P","P","M2","P","R","P","P","P","M2"} };
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-      townButton.setOnAction(new EventHandler<ActionEvent>() {
-          @Override
-          public void handle(ActionEvent event) {
-              parent.displayScreen(MainApp.TOWN_SCREEN);
-          }
-      });
+        for (int i = 0; i < mapLayout.length; i++) {
+            for (int j = 0; j < mapLayout[i].length; j++) {
+                //System.out.println("i: " + i + " j: " + j);
+                // switch(mapLayout[i][j]) {
+                //     case "P":
+                //         //Make Plain
+                //         mapGrid.add(new Button("P"), j, i);
+                //         break;
+                //     case "R":
+                //         //Make River
+                //         mapGrid.add(new Button("R"), j, i);
+                //         break;
+                //     case "T":
+                //         mapGrid.add(townButton, j, i);
+                //         break;
+                //     case "M1":
+                //         //Make Mountain1
+                //         mapGrid.add(new Button("M1"), j, i);
+                //         break;
+                //     case "M2":
+                //         //Make Mountain2
+                //         mapGrid.add(new Button("M2"), j, i);
+                //         break;
+                //     case "M3":
+                //         //Make Mountain3
+                //         mapGrid.add(new Button("M3"), j, i);
+                //         break;
+                // }
+                final String layoutString = mapLayout[i][j];
+                Button newButton = new Button(mapLayout[i][j]);
+                newButton.setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent event) {
+                        if (layoutString.equals("Town"))
+                            parent.displayScreen(MainApp.TOWN_SCREEN);
+                        int j = (int) newButton.getLayoutX();
+                        int i = (int) newButton.getLayoutY();
+                        j /= 63;
+                        i = (i - 13) / 52;
+                        System.out.println("i: " + i + " j: " + j);
+                        // processTile(i, j);
+                    }
+                });
+                mapGrid.add(newButton, j, i);
+            }
+        }
+
     }
+
+    // public void processTile(int i, int j) {
+    //     parent.game.getLandSelectManager().buy(i, j);
+    // }
 
     /* Example method to navigate to a different screen
     @FXML
