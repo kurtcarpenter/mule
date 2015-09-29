8443006973eb5a51be92c4c10e7b74da09a97c86
Kunaal Naik <laanuk13@gmail.com>
2015-09-29 15:56:39 -0400

    gambling / reset view fixes

--- src/main/java/team/game/PubManager.java
+++ src/main/java/team/game/PubManager.java
@@ -3,25 +3,29 @@ package team.game;
 import java.util.List;
 import team.config.Player;
 import team.game.TurnManager;
+import team.game.TimerManager;
 import java.util.Random;
 
 public class PubManager {
   private List<Player> players;
   private TurnManager turnManager;
+  private TimerManager timerManager;
   private Random r;
 
   private static final int[] ROUND_BONUS = {0, 50, 50, 50, 100, 100, 100, 100, 150, 150, 150, 150, 200};
 
-  public PubManager(List<Player> players, TurnManager turnManager) {
+  public PubManager(List<Player> players, TurnManager turnManager, TimerManager timerManager) {
     this.players = players;
     this.turnManager = turnManager;
+    this.timerManager = timerManager;
     r = new Random(System.currentTimeMillis());
   }
 
   public void gamble() {
-    int timeBonus = 60; // Need Timer code here
-    int moneyBonus = ROUND_BONUS[turnManager.getCurrentTurn()] * r.nextInt(timeBonus);
-    turnManager.getCurrentPlayer().incScore(moneyBonus);
-    turnManager.advanceStep();
+    int timeBonus = timerManager.getTime();
+    int moneyBonus = ROUND_BONUS[turnManager.getCurrentTurn()] + r.nextInt(timeBonus);
+    moneyBonus = moneyBonus > 250 ? 250 : moneyBonus;
+    turnManager.getCurrentPlayer().setMoney(moneyBonus);
+    timerManager.resetTimer();
   }
 }
