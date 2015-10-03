be73ff3123ca312ea4e79dc9c3f36b801ec9b4aa
ndass6 <ND@USComputers.net>
2015-10-03 15:34:41 -0400

    Player must enter unique name

--- src/main/java/team/screens/instances/PlayerConfigController.java
+++ src/main/java/team/screens/instances/PlayerConfigController.java
@@ -38,34 +38,49 @@ public class PlayerConfigController extends AScreen {
 
     private List<String> colorsUsed;
 
+    private List<String> namesUsed;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         colorsUsed = new ArrayList<>();
+        namesUsed = new ArrayList<>();
         nextButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
+                boolean nameExists = false;
                 String name = nameField.getText();
                 String color = colorBox.getSelectionModel().getSelectedItem() == null ? "Red" :
                     colorBox.getSelectionModel().getSelectedItem().toString();
                 String race = raceBox.getSelectionModel().getSelectedItem() == null ? "Flapper" :
                     raceBox.getSelectionModel().getSelectedItem().toString();
-                colorsUsed.add(color);
-                parent.config.getPlayers().add(new Player(nameField.getText(),
-                    Player.PlayerRace.valueOf(race.toUpperCase()),
-                    Player.PlayerColor.valueOf(color.toUpperCase())));
-                // System.out.println(parent.config.getPlayers().size());
-                if (parent.config.getSettings().getNumPlayers() <= parent.config.getPlayers().size()) {
-                    parent.game.setCurrentState(GameState.LAND_SELECT);
-                    parent.game.getTurnManager().setState(GameState.LAND_SELECT);
-                    parent.displayScreen(MainApp.MAINMAP_SCREEN);
+                
+                if (namesUsed.contains(name))
+                    nameExists = true;
+
+                if (!nameExists) {
+                    namesUsed.add(name);
+                    colorsUsed.add(color);
+                    parent.config.getPlayers().add(new Player(nameField.getText(),
+                        Player.PlayerRace.valueOf(race.toUpperCase()),
+                        Player.PlayerColor.valueOf(color.toUpperCase())));
+                    // System.out.println(parent.config.getPlayers().size());
+                    if (parent.config.getSettings().getNumPlayers() <= parent.config.getPlayers().size()) {
+                        parent.game.setCurrentState(GameState.LAND_SELECT);
+                        parent.game.getTurnManager().setState(GameState.LAND_SELECT);
+                        parent.displayScreen(MainApp.MAINMAP_SCREEN);
+                    } else {
+                        nameField.setText("Player " + (parent.config.getPlayers().size() + 1));
+                        playerNumText.setText("Player " + (parent.config.getPlayers().size() + 1));
+                        raceBox.setValue("Flapper");
+                        ObservableList<String> colors = colorBox.getItems();
+                        while (colorsUsed.contains(colors.get(0)))
+                            colors.remove(0);
+                        colorBox.setValue(colors.get(0));
+                        parent.displayScreen(MainApp.PLAYER_CONFIG_SCREEN);
+                    }
                 } else {
-                    nameField.setText("Player " + (parent.config.getPlayers().size() + 1));
-                    raceBox.setValue("Flapper");
-                    ObservableList<String> colors = colorBox.getItems();
-                    while (colorsUsed.contains(colors.get(0)))
-                        colors.remove(0);
-                    colorBox.setValue(colors.get(0));
-                    parent.displayScreen(MainApp.PLAYER_CONFIG_SCREEN);
+                    nameField.setText("Name already used.");
+                    nameField.requestFocus();
                 }
             }
         });
