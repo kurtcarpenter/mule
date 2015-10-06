68eea3c62e30dd5a619fa550325e2cf0864a30f5
ndass6 <ND@USComputers.net>
2015-10-05 21:21:34 -0400

    Added functionality for going to the store

---
+++ src/main/java/team/screens/instances/StoreController.java
@@ -0,0 +1,29 @@
+package team.screens.instances;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+import team.screens.AScreen;
+import team.MainApp;
+import team.config.Player;
+
+public class StoreController extends AScreen {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }
+
+    /* Example method to navigate to a different screen
+    @FXML
+    private void goToMain(ActionEvent event){
+        parent.setScreen(ScreensFramework.MAIN_SCREEN);
+    }*/
+}
