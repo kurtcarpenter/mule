2368bd8e33c90e81ae9619976801a63fcae37c98
Kunaal Naik <laanuk13@gmail.com>
2015-10-27 13:54:56 -0400

    prints

--- src/main/java/team/game/RandomEventManager.java
+++ src/main/java/team/game/RandomEventManager.java
@@ -31,21 +31,29 @@ public class RandomEventManager {
           return;
         }
       }
+      
       switch (event) {
            case 1:  p.setResourceQuantity(Resource.FOOD, p.getFood() + 3);
                     p.setResourceQuantity(Resource.ENERGY, p.getFood() + 2);
+                    System.out.println("YOU JUST RECEIVED A PACKAGE FROM THE GT ALUMNI CONTAINING 3 FOOD AND 2 ENERGY UNITS.");
                     break;
            case 2:  p.setOre(p.getOre() + 2);
+                    System.out.println("A WANDERING TECH STUDENT REPAID YOUR HOSPITALITY BY LEAVING TWO BARS OF ORE.");
                     break;
            case 3:  p.setMoney(randFactor[round] * 8);
+                    System.out.println("THE MUSEUM BOUGHT YOUR ANTIQUE PERSONAL COMPUTER FOR $ 8*m.");
                     break;
            case 4:  p.setMoney(randFactor[round] * 2);
+                    System.out.println("YOU FOUND A DEAD MOOSE RAT AND SOLD THE HIDE FOR $2*m.");
                     break;
            case 5:  p.setMoney(randFactor[round] * -4);
+                    System.out.println("FLYING CAT-BUGS ATE THE ROOF OFF YOUR HOUSE. REPAIRS COST $4*m.");
                     break;
            case 6:  p.setResourceQuantity(Resource.FOOD, p.getFood()/2);
+                    System.out.println("MISCHIEVOUS UGA STUDENTS BROKE INTO YOUR STORAGE SHED AND STOLE HALF YOUR FOOD.");
                     break;
            case 7:  p.setMoney(randFactor[round] * -6);
+                    System.out.println("YOUR SPACE GYPSY INLAWS MADE A MESS OF THE TOWN. IT COST YOU $6*m TO CLEAN IT UP.");
                     break;
        }
     }
